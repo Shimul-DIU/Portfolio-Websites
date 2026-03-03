7dae3fd35b7f91c04b71daa41df59ba1a52f6c8b
MD.Shimul <md.shimuldiu@gmail.com>
2026-03-03 23:37:53 +0600

    add toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    let [open ,setOpen]=useState(false)
     return (
         <div>
             <nav className='w-11/12 mx-auto shadow-2xl '>
-                <div className='flex items-center rounded-sm   flex-col  md:flex-row justify-around bg-white   transition-all duration-500 '>
+                <div className='flex items-center rounded-sm   md:flex-row justify-around bg-white   transition-all duration-500 '>
                    <div>
                         <a className='text-2xl font-bold basis-1/3' href="/">Md. Shimul</a>
                    </div>
-                    <ul className='basis-2/3 flex border-b-2 border-b-transparent  justify-around'>
-                        <li className=' hover:border-b-2  md:p-5 border-blue-500'>
+                   <button className='md:hidden' onClick={()=>{setOpen(!open)}}>
+                    {open?<svg  className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg"  height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>:<svg className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>}
+                   </button>
+                    {open && (<ul className='hidden md:flex flex-col '>
+                        <li className=' hover:border-b-3  md:p-5 border-blue-500'>
                             <a href="/">Home</a>
                         </li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/about">About</a></li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/services">Skill</a></li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/contact">Contact</a></li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/contact">Projects</a></li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/contact">Resume</a></li>
-                        <li className='hover:border-b-2 md:p-5'><a href="/contact">Contact</a></li>
-                    </ul>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/about">About</a></li>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/services">Skill</a></li>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Contact</a></li>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Projects</a></li>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Resume</a></li>
+                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Contact</a></li>
+                    </ul>)}
                     
                 </div>
             </nav>
