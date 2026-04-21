f6122cfe68195f0fe48296e94504cdd0c213a60c
MD.Shimul <md.shimuldiu@gmail.com>
2026-04-21 23:58:09 +0600

    add router and svg icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/shimul.png';
 const Navbar = () => {
     let [open ,setOpen]=useState(false)
     return (
         <div>
-            <nav className='w-11/12 mx-auto shadow-2xl '>
-                <div className='flex items-center rounded-sm   md:flex-row justify-around bg-white   transition-all duration-500 '>
+            <h1>My Portfolio</h1>
+            <nav className='w-11/12 mx-auto '>
+                <div className='flex flex-col items-center rounded-sm  md:flex-row justify-around bg-white   transition-all duration-500 '>
                    <div>
-                        <a className='text-2xl font-bold basis-1/3' href="/">Md. Shimul</a>
+                        <img src={logo} alt="shimul logo" width="80px" height="80px" />
                    </div>
                    <button className='md:hidden' onClick={()=>{setOpen(!open)}}>
-                    {open?<svg  className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg"  height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>:<svg className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>}
+                    {open?<svg  className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg"  height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>:
+                    <svg className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>}
                    </button>
-                    {open && (<ul className='hidden md:flex flex-col '>
-                        <li className=' hover:border-b-3  md:p-5 border-blue-500'>
-                            <a href="/">Home</a>
-                        </li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/about">About</a></li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/services">Skill</a></li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Contact</a></li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Projects</a></li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Resume</a></li>
-                        <li className='border-b-2 border-b-transparent hover:border-b-3 hover:border-b-blue-500 md:p-5'><a href="/contact">Contact</a></li>
-                    </ul>)}
-                    
+                    <div className={`flex flex-col items-center md:flex-row justify-around bg-white  w-full md:w-auto absolute md:static left-0  transition-all duration-500 ${open?'top-20':'top-[-490px]'}`}>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/services">Services</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/resume">Resume</Link>
+
+                    </div>
                 </div>
             </nav>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/main.css'
-
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+let router=createBrowserRouter([
+  {
+     path:'/',
+    element:<App></App>
+   },
+   {
+    path:'about',
+   element:<App></App>,
+   },
+   {
+    path:'services',
+   element:<App></App>,
+   },
+   {
+    path:'contact',
+   element:<App></App>,
+   },
+   {
+    path:'projects',
+    element:<App></App>,
+    },
+    {
+      path:'resume',
+      element:<App></App>,
+   }
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter router={router}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
